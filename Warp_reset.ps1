f924aaf4f5cb7e8a6f5e78e0caca22760dd2b2b3
ImmaGundam <120499600+ImmaGundam@users.noreply.github.com>
2025-06-29 09:42:17 -0400

    Add files via upload
    Warp Terminal AI Counter Reset Script in PowerShell
    Windows 10 & 11
    
    You will need to replace the path to locate the warp directory on line 9 and 16 with the location of your warp directory.
    Script will reset the AI counter, delete analytics except your terminal history, and rerun the app. 
    
    Made to simplify coding automation.

---
+++ Warp_reset.ps1
@@ -0,0 +1,17 @@
+﻿# Script to clean up files in AppData\Local\warp\Warp\data
+# Script made to simplify automation. Will stop, delete logs, and re-run Warp
+
+# Stops warp process
+Stop-Process -Name "warp" -Force
+
+# Sets target path to locate files
+# Replace USER with your folder path
+$targetPath = "C:\Users\USER\AppData\Local\warp\Warp\data"
+
+# Removes sqllite files containing usage logs and analytics
+Get-ChildItem -Path $targetPath -File | Where-Object { $_.Name -ne "warp.sqlite" } | Remove-Item -Force
+
+# Sets location for warp to re-run file
+# Replace with your filepath
+Set-Location "C:\Program Files\Warp\"
+Start-Process ".\warp.exe"
